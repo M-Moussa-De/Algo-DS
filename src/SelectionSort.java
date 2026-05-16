0c97ed7d324a1cc9f304004ba0c78be084dc5ad5
M-Moussa-De <mamo1611@hotmail.com>
2026-05-16 14:48:16 +0200

    Edit class

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,45 +1,34 @@
 // T_best = T_avg = T_worst = O(n^2) for the selection sort algorithm
 // Gaussian => (n + 1) / 2
 
-void main() {
-  int[] array = randomArr(1000);
+public class SelectionSort {
+    void main() {
+        int[] array = Utils.randomArr(1000);
 
-    print(array);
-    long startTime = System.currentTimeMillis();
-    selectionSort(array);
-    long endTime = System.currentTimeMillis();
-    print(array);
-    System.out.println("It took " + (endTime - startTime) + " ms");}
+        Utils.print(array);
+        long startTime = System.currentTimeMillis();
+        selectionSort(array);
+        long endTime = System.currentTimeMillis();
+        Utils.print(array);
+        System.out.println("It took " + (endTime - startTime) + " ms");
+    }
 
-void selectionSort(int[] array) {
+    void selectionSort(int[] array) {
 
-    for (int i = 0; i < array.length - 1; i++) {
-        int curr = array[i];
+        for (int i = 0; i < array.length - 1; i++) {
+            int curr = array[i];
 
-        for (int j = i + 1; j < array.length; j++) {
-            if(array[j] < curr) swap(array, i ,j);
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < curr) swap(array, i, j);
+            }
         }
+
     }
 
-}
-
-void swap(int[] arr, int index1, int index2) {
-    int temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
-}
-
-public static int[] randomArr(int size) {
-    Random random = new Random();
-    int[] arr = new int[size];
-    for (int i = 0; i < arr.length; i++) {
-        arr[i] = random.nextInt(size);
+    void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 
-    return arr;
-}
-
-void print(int[] array) {
-    Arrays.stream(array).forEach(x -> System.out.print(x + ", " ));
-    System.out.println();
 }
