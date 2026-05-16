9c7d734fef13560ae9c364d36cff19733807696d
M-Moussa-De <mamo1611@hotmail.com>
2026-05-16 14:22:33 +0200

    SelectionSort

---
+++ src/SelectionSort.java
@@ -0,0 +1,44 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+  int[] array = randomArr(1000);
+
+    print(array);
+    long startTime = System.currentTimeMillis();
+    selectionSort(array);
+    long endTime = System.currentTimeMillis();
+    print(array);
+    System.out.println("It took " + (endTime - startTime) + " ms");}
+
+void selectionSort(int[] array) {
+
+    for (int i = 0; i < array.length - 1; i++) {
+        int curr = array[i];
+
+        for (int j = i + 1; j < array.length; j++) {
+            if(array[j] < curr) swap(array, i ,j);
+        }
+    }
+
+}
+
+void swap(int[] arr, int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+public static int[] randomArr(int size) {
+    Random random = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.length; i++) {
+        arr[i] = random.nextInt(size);
+    }
+
+    return arr;
+}
+
+void print(int[] array) {
+    Arrays.stream(array).forEach(x -> System.out.print(x + ", " ));
+    System.out.println();
+}
